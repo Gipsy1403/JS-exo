8af3e4117a39538d01ea2c581202bd082432b135
Gipsy Rouaret <gipsy.rouaret@orange.fr>
2024-10-29 10:55:21 +0100

    sauvegarde exo 1 et 2

---
+++ assets/javascript/images.js
@@ -0,0 +1,6 @@
+let mainImage=document.getElementById("mainImage");
+let thumbnail=document.getElementsByClassName("thumbnail");
+
+thumbnail[0].addEventListener("click"() =>{
+      mainImage.setAttribute("src", "/assets/image/event-01.jpg")
+})
